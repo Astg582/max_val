8cf86ce1cd9c2c6aeeb2464ed1e230c5e425c1ec
Astg582 <109224508+Astg582@users.noreply.github.com>
2023-07-29 17:20:04 +0400

    Add files via upload

--- max_val.c
+++ max_val.c
@@ -10,20 +10,44 @@
 		}
 	}
 
-	printf(" Max value in array is:%d\n", max);
+	printf("This is the biger number\n%d\n", max) ;
  }
 
+int min_val(int arr[])
+{
+        int min = arr[0];
+        for(int i = 0; i < sizeof(arr[i]); i++)
+        {
+                if (arr[i] < min) {
+                        min = arr[i];
+                }
+        }
+        printf("this is the smallest number:\n%d\n", min) ;
+}
+
+ 
 
 
 int main(){
   
-  int val_arr[5] = {};
+  int val_arr[5];
   printf("enter multiple numbers\n");
   for (int j = 0; j < 5; ++j){
   scanf("%d", &val_arr[j]);
   
   }
  
-int max = max_val ( val_arr );
+ printf("if you need largest value write 'm' if you need the nmaller wallest write 's' \n");
+	char answ;
+ scanf(" %c", &answ);
+  
+ if(answ == 'm')
+ {
+	 int max = max_val(val_arr);
+ }else if(answ == 's'){
+	 int min = min_val(val_arr);
+ }else{
+	 int max = max_val(val_arr);
+	 int min = min_val(val_arr);
 }
-
+}
